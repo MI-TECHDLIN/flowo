7048afce8baa604d11224d7498f22b861878a001
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-04-26 03:26:00 +0100

    modified: replaced prioritystate with statefulwidget

--- lib/features/tasks/widgets/task_/task_priority.dart
+++ lib/features/tasks/widgets/task_/task_priority.dart
@@ -2,49 +2,69 @@ import 'package:flutter/material.dart';
 
 import 'package:flowo/constants/constant.dart';
 
-Widget Taskpriority(
-  Function state,
-  int bordercolor,
-  int backgroundcolor,
-  String label,
-) {
-  bool selected = false;
+class TaskPriority extends StatefulWidget {
+  TaskPriority({
+    required this.label,
+    required this.backgroundcolor,
+    required this.bordercolor,
+    required this.pointercolor,
+  });
+  int bordercolor;
+  int backgroundcolor;
+  int pointercolor;
+  String label;
 
-  return GestureDetector(
-    onTap: () {
-      state(() {
-        selected = !selected;
-      });
-    },
-    child: Container(
-      height: 40,
-      width: 106,
-      decoration: BoxDecoration(
-        border: BoxBorder.all(
-          color: selected == false
-              ? prioritybordercolor(0xB7F5F8FA)
-              : prioritybordercolor(0xFFEF4444),
-        ),
-        color: selected == false
-            ? prioritybackgroundcolor(0xAAF5F8FA)
-            : prioritybackgroundcolor(0xffFFDCDC),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 8,
-            width: 8,
-            decoration: BoxDecoration(
-              color: prioritypointercolor(0xffEF4444),
-              borderRadius: BorderRadius.circular(20),
-            ),
+  @override
+  State<TaskPriority> createState() => _TaskPriorityState();
+}
+
+class _TaskPriorityState extends State<TaskPriority> {
+  bool selected = false;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Container(
+        height: 40,
+        width: 106,
+        decoration: BoxDecoration(
+          border: BoxBorder.all(
+            color: selected == false
+                ? prioritybordercolor(0xB7F5F8FA)
+                : prioritybordercolor(0xFFEF4444),
           ),
-          SizedBox(width: 5),
-          Text('High'),
-        ],
+          color: selected == false
+              ? prioritybackgroundcolor(0xAAF5F8FA)
+              : prioritybackgroundcolor(0xffFFDCDC),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 8,
+              width: 8,
+              decoration: BoxDecoration(
+                color: prioritypointercolor(0xffEF4444),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            SizedBox(width: 5),
+            Text(
+              widget.label,
+              style: TextStyle(
+                color: selected == false
+                    ? Colors.black
+                    : Color(widget.pointercolor),
+              ),
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
