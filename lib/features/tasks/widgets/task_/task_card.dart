0115bbd68a5a617e64bcdbc7b531485570c85476
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-04-28 12:08:40 +0100

    fuxed: card priority text fixed from PDTO

--- lib/features/tasks/widgets/task_/task_card.dart
+++ lib/features/tasks/widgets/task_/task_card.dart
@@ -1,5 +1,6 @@
 import 'package:flowo/data/models/task_model.dart';
 import 'package:flowo/features/tasks/taskfunctions.dart';
+import 'package:flowo/features/tasks/widgets/task_/task_priority.dart';
 import 'package:flowo/main.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -24,6 +25,26 @@ class TaskCard extends StatelessWidget {
     return Colors.grey;
   }
 
+  Color textcolor(int priority) {
+    if (priority == 3)
+      return Color(0xffEF4444);
+    else if (priority == 2)
+      return Color(0xffF5A34A);
+    else if (priority == 1)
+      return Color(0xff6B7C8F);
+    return Colors.grey;
+  }
+
+  Color bgcolor(int priority) {
+    if (priority == 3)
+      return Color(0xffFFDCDC);
+    else if (priority == 2)
+      return Color(0xffFFEDD5);
+    else if (priority == 1)
+      return Color(0xffF5F8FA);
+    return Colors.grey;
+  }
+
   String textpriority(int priority) {
     switch (priority) {
       case 1:
@@ -31,7 +52,7 @@ class TaskCard extends StatelessWidget {
       case 2:
         return 'Medium';
       case 3:
-        return "HIgh Priority";
+        return "High Priority";
       default:
         return 'Normal';
     }
@@ -121,10 +142,13 @@ this function is a widgetstate constraint used to get statecolor
                             height: 23,
                             width: 90,
                             decoration: BoxDecoration(
-                              color: Color(0xFFFFDCDC),
+                              color: bgcolor(task.priority),
                               borderRadius: BorderRadius.circular(8),
                             ),
-                            child: Text('High Priority'),
+                            child: Text(
+                              textpriority(task.priority),
+                              style: TextStyle(color: textcolor(task.priority)),
+                            ),
                           ),
                           SizedBox(width: 30),
                           Text('Work'),
