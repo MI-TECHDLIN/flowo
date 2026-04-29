956ff64470dbb6f83a248943a481a682648b7b4b
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-04-30 00:33:06 +0100

    modified: time parameter added to taskmodel and factory firestore

--- lib/data/models/task_model.dart
+++ lib/data/models/task_model.dart
@@ -1,41 +1,46 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flowo/constants/constant.dart';
 
 class TaskModel {
   final String id;
   final String title;
   final String description;
   final bool isCompleted;
-  final int priority; // 1 = high, 2 = medium, 3 = low
+  final int priority;
+  final TimeOfDay time;
   final DateTime createdAt;
-
   TaskModel({
     required this.id,
     required this.title,
-    this.description = '',
+    this.time = consttime,
+    this.description =
+        '', //description would be optional, there would be a default description
     this.isCompleted = false,
-    this.priority = 2,
+    this.priority = 2, //user priority would be measured by 2
     required this.createdAt,
   });
-
-  // Convert Firestore document → TaskModel
+  //taskmodel converted to a firestore
   factory TaskModel.fromFirestore(DocumentSnapshot doc) {
-    final data = doc.data() as Map<String, dynamic>;
+    final firestoredata = doc.data() as Map<String, dynamic>;
     return TaskModel(
       id: doc.id,
-      title: data['title'] ?? '',
-      description: data['description'] ?? '',
-      isCompleted: data['isCompleted'] ?? false,
-      priority: data['priority'] ?? 2,
-      createdAt: (data['createdAt'] as Timestamp).toDate(),
+
+      title: firestoredata['title'] ?? '',
+      time: firestoredata['time'] ?? '',
+      description: firestoredata['description'] ?? '',
+      isCompleted: firestoredata['isCompleted'] ?? false,
+      priority: firestoredata['priority'] ?? 2,
+      createdAt: (firestoredata['createdAt'] as Timestamp).toDate(),
     );
   }
-
   // Convert TaskModel → Map to save to Firestore
   Map<String, dynamic> toFirestore() {
     return {
       'title': title,
+      'time': time,
       'description': description,
-      'isCompleted': isCompleted,
+      'isCompleated': isCompleted,
       'priority': priority,
       'createdAt': Timestamp.fromDate(createdAt),
     };
@@ -54,6 +59,7 @@ class TaskModel {
       description: description ?? this.description,
       isCompleted: isCompleted ?? this.isCompleted,
       priority: priority ?? this.priority,
+      time: time,
       createdAt: createdAt,
     );
   }
