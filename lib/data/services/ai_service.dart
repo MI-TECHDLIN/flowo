5365c5bb84e2f42f6a68d395fd8b416b1b0dba36
MI-TECHDLIN <ezechukwumiracle52@gmail.com>
2026-05-11 01:56:40 +0100

    feat: conditions to track each call

--- lib/data/services/ai_service.dart
+++ lib/data/services/ai_service.dart
@@ -1,3 +1,5 @@
+//TODO:make sure i switch the localhost to flowo-fastapi cli
+
 import 'dart:convert';
 
 import 'package:flowo/data/models/suggestiion_response.dart';
@@ -13,14 +15,18 @@ this function bascially helps get a two way request
 - it sends to fastapi and get a future response backe through asychronous flow
 
 """;
+
+    // to fastapi
     var response = await http.post(
       Uri.http(_baseurl, '/suggestions'),
       headers: {'content-Type': 'application/json'},
       body: jsonEncode(tasks.map((t) => t.toJson()).toList()),
     );
 
+    //response
     var data = jsonDecode(response.body);
 
+    print(SuggestionResponse.fromJson(data));
     return SuggestionResponse.fromJson(data);
   }
 }
